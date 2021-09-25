d63a79e8e189391e5fb60d71fc2ea3b0502a671c
natalia-se <flaunder1000@gmail.com>
2021-09-25 20:25:11 +0200

    add conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+const condition = true;
+
+// if (condition){
+//     console.log("Sant!")
+// } else {
+//     console.log("Falskt")
+// }
+
+// condition ? console.log("sant") : console.log("falsk");
+
+const age = 18;
+
+if(age < 13){
+    console.log("Not a teenager yet!");
+} else if(age >= 13 && age <= 19){
+    console.log("Teenager");
+}else {
+    console.log("The teens are over");
+}
